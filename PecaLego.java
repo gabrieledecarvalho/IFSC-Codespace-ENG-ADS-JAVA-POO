5648a93ec823fb09b374ff10d318b84012e2f51f
Gabriele de Carvalho <carvalhodegabriele@gmail.com>
2024-09-27 00:15:22 +0000

    Criando um programa PecaLego.java

---
+++ PecaLego.java
@@ -0,0 +1,8 @@
+// programa sobre objeto da apresentação da aula
+
+public class PecaLego { // classe publica PecaLego
+    public static void main(String[] args) { // método main.....
+        PecaLego minhaPeca = new PecaLego(); // objeto minhaPeca sendo criada no molde PecaLego(), não estamos tipando variáveis ainda
+        System.out.println("Uma peça Lego foi criada!");
+    }
+}
