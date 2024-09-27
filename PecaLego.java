b7d519a8e5ce2fb49b28b99e75242065becca858
Gabriele de Carvalho <carvalhodegabriele@gmail.com>
2024-09-27 01:01:23 +0000

    Modificando o arquivo PecaLego com atributos agora

--- PecaLego.java
+++ PecaLego.java
@@ -1,8 +1,24 @@
-// programa sobre objeto da apresentação da aula
+public class PecaLego { // classe pública PecaLego
+    // Atributo (característica da peça)
+    int numeroDePinos;
+    int largura;
 
-public class PecaLego { // classe publica PecaLego
-    public static void main(String[] args) { // método main.....
-        PecaLego minhaPeca = new PecaLego(); // objeto minhaPeca sendo criada no molde PecaLego(), não estamos tipando variáveis ainda
-        System.out.println("Uma peça Lego foi criada!");
-    }
+    public static void main(String[] args) {
+        // Criando um objeto (uma peça Lego real)
+        PecaLego minhaPeca = new PecaLego();
+        PecaLego minhaPeca2 = new PecaLego(); // criando um novo objeto dentro da mesma classe, posso utilizar os mesmos atributos definidos na classe com valores diferentes
+
+        // Definindo o atributo (característica da peça)
+        minhaPeca.numeroDePinos = 4; // quando tem pontinho significa que tem a ver com atributo da peça 
+        minhaPeca.largura = 5;
+        
+        // utilizando um atribudo da classe na peça número 2
+        minhaPeca2.numeroDePinos = 6;
+        
+        // Mostrando o atributo da peça
+        System.out.println("Minha peça Lego tem " + minhaPeca.numeroDePinos + " pinos"); // inprimindo minhaPeca + atributo dela
+        System.out.println("Minha peça Lego tem " + minhaPeca.largura + " de largura"); // inprimindo minhaPeca + atributo dela
+
+        System.out.println("Minha peça Lego tem " + minhaPeca2.numeroDePinos + " pinos"); // inprimindo minhaPeca + atributo dela
+    } 
 }
