9be9edeee7adc9a5ded8b4ab959be115fa1cd679
Gabriele de Carvalho <carvalhodegabriele@gmail.com>
2024-10-02 00:35:40 +0000

    modificando o arquivo EX1PecaLego.java

--- EX1PecaLego.java
+++ EX1PecaLego.java
@@ -2,7 +2,7 @@ public class EX1PecaLego {
     // Atributos (características da peça)
     String cor;
     String formato;
-    int numeroDePinos;
+    int numeroDePinos, altura;
 
     // Métodos (ações que a peça pode realizar)
     public void mostrarDetalhes() {
@@ -13,20 +13,32 @@ public class EX1PecaLego {
         System.out.println("Peça Lego " + cor + " girou 90 graus!");
     }
 
+    public void rotacao() {
+        System.out.println("Peça Lego " + cor + " rotacionou 180 graus!");
+    }
+
     public static void main(String[] args) {
         // Criando objetos (peças Lego reais)
-        PecaLego pecaVermelha = new PecaLego();
-        PecaLego pecaAzul = new PecaLego();
+        EX1PecaLego pecaVermelha = new EX1PecaLego();
+        EX1PecaLego pecaAzul = new EX1PecaLego();
+        EX1PecaLego pecaAmarela = new EX1PecaLego();
 
         // Definindo características da peça vermelha
         pecaVermelha.cor = "Vermelha";
         pecaVermelha.formato = "Retangular";
         pecaVermelha.numeroDePinos = 4;
+        pecaVermelha.altura = 3;
 
         // Definindo características da peça azul
         pecaAzul.cor = "Azul";
         pecaAzul.formato = "Quadrado";
         pecaAzul.numeroDePinos = 2;
+        pecaVermelha.altura = 5;
+
+        pecaAmarela.cor = "Amarela";
+        pecaAmarela.formato = "Retangular"
+        pecaAmarela.numeroDePinos = 8;
+        pecaAmarela.altura = 2;
 
         // Fazendo as peças realizarem ações
         pecaVermelha.mostrarDetalhes();
